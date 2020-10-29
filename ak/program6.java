e31dc705119f212922c6357b53c88b5bd2c7aa75
athulkrishnas <73597285+athulkrishnas@users.noreply.github.com>
2020-10-29 15:02:49 +0530

    Update program6.java

--- ak/program6.java
+++ ak/program6.java
@@ -1,19 +1,23 @@
 import java.io.*;
-public class Program6{
-   public static void main(String[] args){
-	int n=7;
-		Program6 fact = new Program6();
-		fact.Fact(n);
-	}
-	void Fact(int n){
-		int i=1,a=1;
-		while(i<=n)
-		{
-			a*=i;
-			i++;
-		}
-		System.out.println("Factorial of "+n+" = "+a);
-	}
 
+public class program6{
+
+  public static void main(String[] args) {
+
+    
+    int number = 47;
+
+    System.out.print("Factors of " + number + " are");
+
+    
+    for (int i = 1; i <= number; ++i) {
+
+      
+      if (number % i == 0) {
+        System.out.print(i + " ");
+      }
+    }
+  }
 }
+
 		
